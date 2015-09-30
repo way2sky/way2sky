b0838a9f640247d34df32047bcffbdf3e13c9680
Xuan <xuan.li@way2sky.com>
2015-09-30 02:54:00 -0400

    add way2sky original site p1

---
+++ Projects/hertzian/js/switchBG.js
@@ -0,0 +1,17 @@
+<!--
+	function switchCSS() {
+		var myLink = document.getElementsByTagName('link')[0];
+		
+		if (window.screen.availWidth == 2560) {
+			if(myLink){myLink.setAttribute('href','style2560.css',0);}
+		} else if (window.screen.availWidth == 1920) {
+			if(myLink){myLink.setAttribute('href','style1920.css',0);}
+		} else if (window.screen.availWidth == 1440) {
+			if(myLink){myLink.setAttribute('href','style1440.css',0);}
+		} else if (window.screen.availWidth == 1280) {
+			if(myLink){myLink.setAttribute('href','style1280.css',0);}
+		} else if (window.screen.availWidth == 1024) {
+			if(myLink){myLink.setAttribute('href','style1024.css',0);}
+		} 
+	}
+//-->
